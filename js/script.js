e340d56a28f667be728fa09374429d00cc5a56be
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-12-01 16:22:28 +0900

    slickからsplideに変更

--- js/script.js
+++ js/script.js
@@ -1,73 +1,78 @@
-// ページ内スクロール
-$('a[href^="#"]').click(function () {
-  const speed = 600;
-  let href = $(this).attr("href");
-  let target = $(href == "#" || href == "" ? "html" : href);
-  let position = target.offset().top;
-  $("body,html").animate({ scrollTop: position }, speed, "swing");
-  return false;
+var supportsPassive=false;try{var opts=Object.defineProperty({},'passive',{get:function(){supportsPassive=true;}});window.addEventListener("testPassive", null, opts);window.removeEventListener("testPassive", null, opts);} catch (e) {}
+// ハンバーガーメニュー---------------------------------------------------
+const ham = document.querySelector(".js-l-header__burger-btn"); //単一の要素を取得
+const menu = document.querySelector(".js-l-header__burger-menu");
+const body = document.querySelector("body");
+/*---スマホのときはtouchstart---*/
+let flag2 = false;
+ham.addEventListener("touchstart",{},supportsPassive?{passive:true}:false, function () { //要素が押されたら
+  flag2 = true;
+  ham.classList.toggle("active"); //toggle 切り替え
+  if (ham.classList.contains("active")) { //判定
+    menu.classList.add("active"); //追加
+    body.classList.add("fixed"); //追加
+  } else {
+    menu.classList.remove("active"); //削除
+    body.classList.remove("fixed"); //削除
+  }
 });
+/*---PCではclick---*/
+ham.addEventListener("click", function () { //要素がクリックされたら
+  if (flag2) {
+    flag2 = false;
+  } else {
+  ham.classList.toggle("active");
+    if (ham.classList.contains("active")) {
+      menu.classList.add("active");
+      body.classList.add("fixed");
+      } else {
+        menu.classList.remove("active");
+        body.classList.remove("fixed");
+  }
+}});
 
-// ハンバーガーメニュー
-$(function () {
-  $(".js-l-header__burger-btn").click(function () {
-    $(this).toggleClass("active");
-
-    if ($(this).hasClass("active")) {
-      $(".js-l-header__burger-menu").addClass("active");
-      $("body").addClass("fixed");
+// ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように-----------------
+const link = document.querySelectorAll(".l-header__burger-menu-link"); //複数の要素を取得
+//イベントオブジェクト生成
+const triggerEvent1 = new Event('touchstart')
+const triggerEvent2 = new Event('click')
+/*---スマホのときはtouchstart---*/
+let flag3 = false;
+link.forEach( //querySelectorAllは配列なのでループさせる必要がある
+  e => e.addEventListener("touchstart",{},supportsPassive?{passive:true}:false, function () { //要素がクリックされたら eventのe
+    flag3 = true;
+    ham.dispatchEvent(triggerEvent1);
+  })
+);
+/*---PCではclick---*/
+link.forEach(
+  e => e.addEventListener("click", function () {
+    if (flag3) {
+      flag3 = false;
     } else {
-      $(".js-l-header__burger-menu").removeClass("active");
-      $("body").removeClass("fixed");
-    }
-  });
-});
-// ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
-$(".l-header__burger-menu-link,a").on("click", function (event) {
-  $(".js-l-header__burger-btn").trigger("click");
-});
+    ham.dispatchEvent(triggerEvent2);
+  }
+}));
 
-$(function () {
-  $("#js-p-works-primary__public-slider").slick({
-    arrows: false,
-    autoplay: false,
-    adaptiveHeight: false,
-    speed: 1000,
-    slidesToShow: 1,
-    centerMode: true,
-    centerPadding: "190px",
-    dots: true,
-    dotsClass: "p-works-primary__slider-dots",
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          centerPadding: "50px",
-        }
-      },
-    ],
-  });
-});
+const target = '#slider1';
+const options = {
+	type : 'loop',
+	speed: 600,
+	padding: '20%',
+	perPage: 1,
+	perMove: 1,
+	wheel: true,
+	releaseWheel: true,
+	trimSpace: false,
+	focus: 'center',
+	updateOnMove: false,
+  gap:20,
+}
+const mySplide = new Splide(target, options);
 
-$(function () {
-  $("#js-p-works-primary__apartment-slider").slick({
-    arrows: false,
-    autoplay: false,
-    adaptiveHeight: false,
-    speed: 1000,
-    slidesToShow: 1,
-    centerMode: true,
-    centerPadding: "190px",
-    dots: true,
-    dotsClass: "p-works-primary__slider-dots",
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          centerPadding: "50px",
-        }
-      },
-    ],
-  });
-});
+mySplide.mount();
 
+const target2 = '#slider2';
+const mySplide2 = new Splide(target2, options);
+
+mySplide2.mount();
